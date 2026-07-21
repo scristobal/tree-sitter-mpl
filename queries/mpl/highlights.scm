ea1bed124cdfaf2d40af16305c1691437f5bfcfb
Samuel Cristobal <samuel.cristobal@protonmail.com>
2026-07-20 21:16:24 -0700

    Overhaul MPL semantic highlighting

--- queries/mpl/highlights.scm
+++ queries/mpl/highlights.scm
@@ -46,6 +46,7 @@
   "bool"
 ] @type.builtin
 
+; Bucket functions and arguments are grammar keywords rather than identifiers.
 [
   "rate"
   "increase"
@@ -62,15 +63,116 @@
 
 (comment) @comment
 
-(plain_identifier) @variable
-(escaped_identifier) @variable
-(param_ident "$" @punctuation.special) @variable.parameter
+; Parameters retain their sigil without allowing the nested identifier to
+; acquire a competing generic variable capture.
+(param_ident
+  "$" @punctuation.special
+  (identifier) @variable.parameter)
+
+; Metric identifiers consist of a dataset and a metric name. A parameterized
+; dataset is already handled by the param_ident pattern above.
+(metric_id
+  dataset: (identifier) @module)
 
 (metric_id
-  dataset: (_) @module
-  name: (_) @function)
+  name: (metric_name
+    (identifier) @function))
 
-(func) @function.call
+; Names introduced for a metric stream.
+(as_clause
+  alias: (metric_name
+    (identifier) @variable))
+
+(compute_rule
+  name: (metric_name
+    (identifier) @variable))
+
+; Directive names are attributes of the query. An unquoted identifier in a
+; directive value is a constant, not another declaration.
+(directive
+  name: (identifier) @attribute)
+
+(directive
+  value: (identifier) @constant)
+
+; Tags and tag references.
+(filter_atom
+  tag: (identifier) @variable.member)
+
+(tags
+  (identifier) @variable.member)
+
+(expr
+  (identifier) @variable.member)
+
+(extend_expr
+  name: (identifier) @variable.member)
+
+(replace_tag
+  tag: (identifier) @variable.member)
+
+(replace_rename
+  name: (identifier) @variable.member
+  tag: (identifier) @variable.member)
+
+(replace_rename_tag
+  name: (identifier) @variable.member
+  tag: (identifier) @variable.member)
+
+; A qualified callable is highlighted component-by-component instead of as a
+; single range, so e.g. prom::rate has a module and a terminal function.
+(module
+  (identifier) @module)
+
+(func
+  (identifier) @function.builtin
+  (#any-of? @function.builtin
+    "abs"
+    "avg"
+    "const"
+    "count"
+    "eq"
+    "gt"
+    "gte"
+    "histogram"
+    "increase"
+    "interpolate_cumulative_histogram"
+    "interpolate_delta_histogram"
+    "last"
+    "linear"
+    "lt"
+    "lte"
+    "max"
+    "min"
+    "neq"
+    "prev"
+    "rate"
+    "sum"))
+
+(func
+  (identifier) @function.call
+  (#not-any-of? @function.call
+    "abs"
+    "avg"
+    "const"
+    "count"
+    "eq"
+    "gt"
+    "gte"
+    "histogram"
+    "increase"
+    "interpolate_cumulative_histogram"
+    "interpolate_delta_histogram"
+    "last"
+    "linear"
+    "lt"
+    "lte"
+    "max"
+    "min"
+    "neq"
+    "prev"
+    "rate"
+    "sum"))
 
 (string) @string
 (interpolation
@@ -107,17 +209,18 @@
   "}"
   "["
   "]"
-  "<"
-  ">"
 ] @punctuation.bracket
 
+; Angle brackets delimit Option<T>; angle comparisons are operators.
+(optional_type
+  ["<" ">"] @punctuation.bracket)
+
 [
-  "=="
-  "!="
-  "<="
-  ">="
-  "<"
-  ">"
+  (cmp)
+  (cmp_re)
+] @operator
+
+[
   "="
   "+"
   "-"
